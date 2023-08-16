13db48ab752e08c1d03dc61479fe986d67691ed5
Lucas Shen dos Santos <lshens@hotmail.com>
2023-08-16 10:52:22 -0300

    small fix on the webapp

--- webapp/src/app/pages/home/home.component.ts
+++ webapp/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Course } from 'src/app/models/course.model';
 
 @Component({
